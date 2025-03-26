e1672828c8f6d85ce53c5391178a3ac33fd8df2c
dindinva <115386469+dindinva@users.noreply.github.com>
2025-03-26 17:06:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,9 @@ st.title("🎈 Gemini chat")
 st.write("Gemini:คุยได้เลยนะ")
 history=[]
 client = genai.Client(api_key="AIzaSyD02r_b6nn1lzkEjA6dCewkDfNCgkY5IIY")
-ask=st.chat_input("ฉัน:")
-if ask:
+
+while True:
+   ask=st.input("ฉัน:")
    history.append("ฉัน:"+ask)
    st.write("ฉัน:"+ask)
    response = client.models.generate_content(
