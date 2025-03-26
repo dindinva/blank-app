5966a9992ce6929f05ee1a292e8cab681a0a54a0
dindinva <115386469+dindinva@users.noreply.github.com>
2025-03-26 16:29:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,12 +8,12 @@ history=""
 client = genai.Client(api_key="AIzaSyD02r_b6nn1lzkEjA6dCewkDfNCgkY5IIY")
 ask=st.chat_input("ฉัน:")
 if ask:
-   history=history+ask
+   history=history+ask+"\n"
    response = client.models.generate_content(
     model="gemini-2.0-flash",
-    contents=ask,
+    contents=history,
 )
    answer=response.text
-   history=history+answer
+   history=history+answer+"\n"
    st.write(history)
 
