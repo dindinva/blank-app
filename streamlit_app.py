06401dedd7bdb201ad8c91f797689b87cc57adc9
dindinva <115386469+dindinva@users.noreply.github.com>
2025-03-26 16:12:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,14 @@ from google import genai
 
 
 st.title("🎈 Gemini chat")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.write("Gemini:คุยได้เลยนะ")
 client = genai.Client(api_key="AIzaSyD02r_b6nn1lzkEjA6dCewkDfNCgkY5IIY")
-ask=st.chat_input("Ask me...")
+ask=st.chat_input("ฉัน:")
 if ask:
    response = client.models.generate_content(
     model="gemini-2.0-flash",
     contents=ask,
 )
 
-   st.write(response.text)
+   st.write_stream("Gemini:"+response.text)
 
