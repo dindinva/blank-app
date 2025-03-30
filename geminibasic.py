d50fb290162cc5e94336eda9789e5431115d5b01
dindinva <115386469+dindinva@users.noreply.github.com>
2025-03-30 21:26:51 +0700

    Create geminibasic.py

---
+++ geminibasic.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from google import genai
+
+
+st.title("🎈 Gemini chat")
+st.write("Gemini:คุยได้เลยนะ")
+client = genai.Client(api_key="AIzaSyD02r_b6nn1lzkEjA6dCewkDfNCgkY5IIY")
+ask=st.text_input("ฉัน:")
+if ask:
+ 
+   response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=ask,
+)
+
+   st.write(response.text)
+
+   
