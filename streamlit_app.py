3af1c1963a91180b7148c96e5df9b3c090493567
dindinva <115386469+dindinva@users.noreply.github.com>
2025-03-30 21:47:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,34 @@
-#gemini chatbot
-
-#import os
-import streamlit as st
 from google import genai
+import streamlit as st
 
-try:
-    #gemini_api_key = os.environ['GOOGLE_GEMINI_KEY']
-    client = genai.Client(api_key="AIzaSyD02r_b6nn1lzkEjA6dCewkDfNCgkY5IIY")
-    model = genai.GenerativeModel('gemini-2.0-flash')
+st.title("Gemini-like clone")
 
-    if "chat" not in st.session_state:
-      st.session_state.chat = model.start_chat(history=[])
-    st.title('Gemini-2.0 chatbot')
+client = genai.Client(api_key="AIzaSyD02r_b6nn1lzkEjA6dCewkDfNCgkY5IIY")
 
-    def role_to_streamlit(role: str) -> str:
-      if role == 'model':
-        return 'assistant'
-      else:
-        return role
+if "genai_model" not in st.session_state:
+    st.session_state["genai_model"] = "gemini-2.0-flash"
 
-    for message in st.session_state.chat.history:
-      with st.chat_message(role_to_streamlit(message.role)):
-        st.markdown(message.parts[0].text)
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-    if prompt := st.chat_input("I possess a well of knowledge. What would you like to know?"):
-      st.chat_message("user").markdown(prompt)
-      response = st.session_state.chat.send_message(prompt)
-      with st.chat_message("assistant"):
-        st.markdown(response.text)
-except Exception as e:
-    st.error(f'An error occurred: {e}')
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-   
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["genai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    
