c6a46886c48c71a15a75b7e8e5c88a1708322b94
dindinva <115386469+dindinva@users.noreply.github.com>
2025-03-31 17:39:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,5 @@ if prompt := st.chat_input("What is up?"):
         history=""
     st.session_state.messages.append({"role": "assistant", "content": response})
     if len(st.session_state.messages)>=6:
-        st.session_state.messages.pop(index=-6)
-        st.session_state.messages.pop(index=-5)
+        st.session_state.messages.pop(0)
+        st.session_state.messages.pop(0)
