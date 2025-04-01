1d26275011441114f51e8f7fe0ef0740761f5ec9
dindinva <115386469+dindinva@users.noreply.github.com>
2025-04-01 17:38:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 #gemini live chat
 import asyncio
 from google import genai
+import streamlit as st
 
 client = genai.Client(api_key="GEMINI_API_KEY", http_options={'api_version': 'v1alpha'})
 model = "gemini-2.0-flash-exp"
@@ -10,14 +11,14 @@ config = {"response_modalities": ["TEXT"]}
 async def main():
     async with client.aio.live.connect(model=model, config=config) as session:
         while True:
-            message = input("User> ")
+            message = st.text_input("User> ")
             if message.lower() == "exit":
                 break
             await session.send(input=message, end_of_turn=True)
 
             async for response in session.receive():
                 if response.text is not None:
-                    print(response.text, end="")
-
-if __name__ == "__main__":
-    asyncio.run(main())
+                    answer=response.text
+                    
+                    st.write(answer)
+asyncio.run(main())
